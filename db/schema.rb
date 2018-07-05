a2cfe085e0d10bc6149f86102b37134292743f6d
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-05 16:52:09 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,11 @@
 ActiveRecord::Schema.define(version: 20180705164625) do
 
   create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_datetime"
     t.integer  "patient_id"
     t.integer  "doctor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   add_index "appointments", ["doctor_id"], name: "index_appointments_on_doctor_id"
