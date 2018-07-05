b90f88e73f598e5ee50bdd38f151639a098c33ee
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-05 17:44:55 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   resources :patients
   resources :appointments, only: [:show]
 
-  Routing Error
-  No route matches [GET] "/patients/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
