c827d411ea23e0f5cfb1f85bd85b4cf97d39c517
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-05 17:29:48 +0000

    automatically backed up by learn

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ActiveRecord::Base
   has_many :appointments
   has_many :doctors, through: :appointments
 
-  def patient_appts
+  def name_appt
     self.name + self.appointments.size
   end
 end
